d46188cf8c671d7e25aa70fd729167e963973144
Daniel Camargo <pererinha@gmail.com>
2009-07-06 23:49:06 -0300

    added js file

---
+++ mailto-anti-spam.js
@@ -0,0 +1,16 @@
+/**
+ * Hide mailto from spam-robots
+ * By Daniel Camargo (http://twitter.com/pererinha | http://danielcamargo.com.br/)
+ * Copyright (c) 2009
+ * Released under the MIT license (MIT-LICENSE.txt)
+ * Demo: http://www.danielcamargo.com.br/stuffs/hide-mailto-spam-robot/
+ * 2009-07-06
+ */
+var config = {
+	at : '@',
+	domain : 'gmail',
+	comercial : 'com'
+}
+function MailtoAntiSpam(destiny){
+	document.write(unescape('%3Cstrong%3E%3Ca%20href%3D%22mailto:' + destiny + config.at + config.domain + config.comercial + '%22%3E' + destiny + config.at + config.domain + config.comercial + '%3C/a%3E%3C/strong%3E'));
+}
